fd8deab16c098f96576d3f54a31a251afc3a34b4
iamcord <sithry.cd@gmail.com>
2018-02-15 14:14:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-const request = require('request');
-
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
+const got = require('got');
+ 
+got('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }).then(response => {
+  console.log(response.body.url);
+  console.log(response.body.explanation);
+}).catch(error => {
+  console.log(error.response.body);
 });
