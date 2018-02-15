fdb28cfaba1a25f8d02cbc990473fdafca6920ce
iamcord <sithry.cd@gmail.com>
2018-02-15 11:13:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
-console.log("lolno!")
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "https://ipinfo.io/json", true);
+xhr.send();
+
+xhr.onreadystatechange = processRequest;
+
+function processRequest(e) {
+  if (xhr.readyState == 4) {
+    var response = JSON.parse(xhr.responseText);
+    console.log(response.ip);
+  }
+}
